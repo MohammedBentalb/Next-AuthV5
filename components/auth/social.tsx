f69d1e600838ee842287943ff62d628a951b6fc9
mohammed bentalb <mohammedbentalb2000@gmail.com>
2024-11-06 22:21:18 +0100

    Nothing fancy, just the UI using shadcn

---
+++ components/auth/social.tsx
@@ -0,0 +1,28 @@
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Button } from "../ui/button";
+
+const Social = () => {
+  return (
+    <div className="flex w-full items-center gap-x-2">
+      <Button
+        size={"lg"}
+        className="w-full"
+        variant={"outline"}
+        onClick={() => {}}
+      >
+        <FcGoogle />
+      </Button>
+      <Button
+        size={"lg"}
+        className="w-full"
+        variant={"outline"}
+        onClick={() => {}}
+      >
+        <FaGithub />
+      </Button>
+    </div>
+  );
+};
+
+export default Social;
